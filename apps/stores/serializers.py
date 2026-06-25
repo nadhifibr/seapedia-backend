898397c8cc2714a0cafa2646377ef01cf889b1fd
Nadhif Ibrahim <mnadhifibr@gmail.com>
2026-06-26 01:54:35 +0700

    feat: add recent_products to StoreSerializer to display top 3 active products per store

--- apps/stores/serializers.py
+++ apps/stores/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from .models import Store
 
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        from apps.products.models import Product
+        model = Product
+        fields = ['id', 'name', 'price', 'image_url']
+
 class StoreSerializer(serializers.ModelSerializer):
+    recent_products = serializers.SerializerMethodField()
+
     class Meta:
         model = Store
-        fields = ['id', 'name', 'slug', 'description', 'image_url', 'location', 'created_at']
+        fields = ['id', 'name', 'slug', 'description', 'image_url', 'location', 'created_at', 'recent_products']
         read_only_fields = ['id', 'slug', 'created_at']
+
+    def get_recent_products(self, obj):
+        from apps.products.models import Product
+        products = Product.objects.filter(store=obj, is_active=True).order_by('-created_at')[:3]
+        return SimpleProductSerializer(products, many=True).data
